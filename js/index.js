91fb9019d3deb4665e619fdd44284cf1d213b527
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2021-12-07 02:19:31 -0600

    Reverted last commit due to errors

--- js/index.js
+++ js/index.js
@@ -40,8 +40,3 @@ themeBtn.addEventListener("click", function(){
         setTheme(0)
     }
 })
-
-if ("serviceWorker" in navigator) {
-    // register service worker
-    navigator.serviceWorker.register("service-worker.js");
-  }
